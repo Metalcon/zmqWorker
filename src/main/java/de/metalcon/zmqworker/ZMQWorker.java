4f95457c33e0b3185927ee4e9211951285570281
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-03-27 14:00:54 +0100

    worker made stoppable

--- src/main/java/de/metalcon/zmqworker/ZMQWorker.java
+++ src/main/java/de/metalcon/zmqworker/ZMQWorker.java
@@ -3,6 +3,7 @@ package de.metalcon.zmqworker;
 import java.io.Serializable;
 
 import org.apache.commons.lang3.SerializationUtils;
+import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 import de.metalcon.api.responses.errors.ParsingErrorResponse;
@@ -21,9 +22,10 @@ public class ZMQWorker extends StoppableWorker {
     private static int NUM_WORKERS = 0;
 
     /**
-     * ZeroMQ context
+     * ZeroMQ context<br>
+     * now ZContext (https://github.com/zeromq/jeromq/issues/140)
      */
-    private static ZMQ.Context CONTEXT;
+    private static ZContext CONTEXT;
 
     /**
      * ZeroMQ socket to receive/send messages
@@ -47,30 +49,45 @@ public class ZMQWorker extends StoppableWorker {
      *            endpoint the worker will serve on
      * @param requestHandler
      *            handler for incoming requests
+     * @param context
+     *            ZMQ context to use<br>
+     *            set to <b>null</b> to use the context set (creates a new one
+     *            if none set)
+     * @throws IllegalStateException
+     *             if different context set previously, which is still in use
      */
     public ZMQWorker(
             String endpoint,
             ZMQRequestHandler requestHandler,
-            ZMQ.Context context) {
+            ZContext context) {
         this.endpoint = endpoint;
         this.requestHandler = requestHandler;
-
+        if (context == null) {
+            if (CONTEXT == null) {
+                // TODO log: no context specified, using new context
+                context = new ZContext();
+            }
+            // reuse current context else
+        } else if (CONTEXT != null && CONTEXT != context && NUM_WORKERS > 0) {
+            throw new IllegalStateException(
+                    "different context already set and still in use");
+        }
         CONTEXT = context;
     }
 
-    @Override
-    public boolean start() {
-        if (!running) {
-            NUM_WORKERS += 1;
-
-            socket = CONTEXT.socket(ZMQ.ROUTER);
-            socket.bind(endpoint);
-            socket.setSendBufferSize(100000);
-            socket.setHWM(100000);
-
-            return super.start();
-        }
-        return false;
+    /**
+     * create new ZeroMQ worker<br>
+     * uses the context set or creates a new one
+     * 
+     * @param endpoint
+     *            endpoint the worker will serve on
+     * @param requestHandler
+     *            handler for incoming requests
+     */
+    public ZMQWorker(
+            String endpoint,
+            ZMQRequestHandler requestHandler) {
+        this(endpoint, requestHandler, null);
     }
 
     /**
@@ -78,21 +95,32 @@ public class ZMQWorker extends StoppableWorker {
      */
     @Override
     public void run() {
-        // TODO: use logging
-        System.out.println("i am serving on endpoint: " + endpoint);
-        running = true;
-
         boolean error = false;
         Object request;
         Serializable response;
 
+        NUM_WORKERS += 1;
+
+        socket = CONTEXT.createSocket(ZMQ.ROUTER);
+        socket.bind(endpoint);
+        socket.setSendBufferSize(100000);
+        socket.setHWM(100000);
+        socket.setReceiveTimeOut(10);
+        socket.setLinger(50);
+
+        // TODO: use logging
+        System.out.println("i am serving on endpoint: " + endpoint);
+
         try {
+            running = true;
             while (!stopping) {
                 byte[] clientId = socket.recv();
                 byte[] serializedRequest = socket.recv();
                 if (serializedRequest == null) {
                     // error or shutdown
-                    break;
+                    System.out.println("received" + ", " + running + ", "
+                            + stopping);
+                    continue;
                 }
 
                 byte[] serializedResponse = null;
@@ -149,10 +177,6 @@ public class ZMQWorker extends StoppableWorker {
     @Override
     public void stop() {
         super.stop();
-
-        // close socket to leave listener loop
-        socket.close();
-
         waitForShutdown();
     }
 
@@ -160,11 +184,15 @@ public class ZMQWorker extends StoppableWorker {
      * clean up after shutdown
      */
     protected void cleanUp() {
-        // TODO: close socket if not closed yet
-
         NUM_WORKERS -= 1;
+
         if (NUM_WORKERS == 0) {
-            CONTEXT.term();
+            // TODO log: clearing context
+            CONTEXT.destroy();
+            // TODO log: context cleared
+        } else {
+            CONTEXT.destroySocket(socket);
+            // TODO log: NUM_WORKERS workers left
         }
     }
 }
